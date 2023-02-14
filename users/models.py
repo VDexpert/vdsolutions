a1c6fb8d843b6fd9adcd5b9a757ea8ce32c630aa
vsevo <vdexpert11@yandex.ru>
2023-02-15 02:08:40 +0500

    make fields user phone and country not required and enable control of the fullness of these parameters when displaying a user card on the product page and post

--- users/models.py
+++ users/models.py
@@ -8,9 +8,9 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(verbose_name='Почта', unique=True)
-    phone = models.CharField(verbose_name='Телефон', max_length=20)
-    avatar = models.ImageField(verbose_name='Аватарка', upload_to='users/', help_text=f'рекомендуемый размер 300*300', **NULLABLE )
-    country = models.CharField(verbose_name='Страна', max_length=30)
+    phone = models.CharField(verbose_name='Телефон', max_length=20, **NULLABLE)
+    avatar = models.ImageField(verbose_name='Аватарка', upload_to='users/', help_text=f'рекомендуемый размер 300*300', **NULLABLE)
+    country = models.CharField(verbose_name='Страна', max_length=30, **NULLABLE)
     email_verify = models.BooleanField(default=False)
     trials_update_range_prod = models.IntegerField(verbose_name='Кол-во апдейта ранжирования продукта', default=10)
     stack = models.CharField(verbose_name='Опишите Ваш стэк', max_length=80, **NULLABLE, help_text='максимальное количество символов - 80')
